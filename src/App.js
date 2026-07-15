74048ebfe365b714e424ee1c012559daaef31b76
fabascoder <fabricialvez2@gmail.com>
2026-07-15 17:58:47 -0300

    encontrado cada escola por "codAcesso"

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import escola from './api/escola.json'
 
 export default function App() {
+  const [api, setApi] = React.useState([])
+
+   React.useEffect(() => {
+    setApi(escola)
+  }, []);
+
+  console.log(Array.isArray(api))
+  const escolaFinded = api.find(esc => esc.codAcesso === 1233)
+  console.log()
+
   return (
     <View style={styles.container}>
+      {escolaFinded && <Text>{escolaFinded.nome}</Text>}
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
