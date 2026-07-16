bbb2c03b906b17fe20619cc254e9cb15cb4e9b3b
fabascoder <fabricialvez2@gmail.com>
2026-07-15 22:26:02 -0300

    Renderização dos horarios

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,37 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native-web";
+import { View, StyleSheet, Text, Pressable } from "react-native-web";
 import { GlobalContext } from "../context/GlobalContext";
 
 export const Home = () => {
   const dataContext = React.useContext(GlobalContext);
+  console.log(dataContext.data.escola.horarios);
+
+  function handleHorario({ target }) {
+    console.log(target);
+  }
 
   return (
     <View style={styles.container}>
       {dataContext.data.escola && dataContext.data.funcionario && (
-        <Text>
-          Escola: {dataContext.data.escola.nome} | Funcionario:{" "}
-          {dataContext.data.funcionario.nome}
-        </Text>
+        <View>
+          <Text>Olá, {dataContext.data.funcionario.nome}</Text>
+          <View>
+            {dataContext.data.escola.horarios.map((escola) => {
+              return (
+                <View key={escola.id}>
+                  <Pressable onPress={handleHorario}>
+                    <Text>
+                      {escola.nome} Inicio: {escola.inicio} fim: {escola.fim}
+                    </Text>
+                    <Text>Amostra: {escola.horarioAmostra}</Text>
+                  </Pressable>
+                </View>
+              );
+            })}
+          </View>
+        </View>
       )}
+      <View></View>
     </View>
   );
 };
